4aa47c099acf22c354a43d4b0f85b1191e521411
AndreyG <nba.grinin.96@gmail.com>
2026-06-15 17:53:56 +0700

    add group for markets in event details

--- app/src/main/java/ru/grinin/winlineapp/navigation/MainNavHost.kt
+++ app/src/main/java/ru/grinin/winlineapp/navigation/MainNavHost.kt
@@ -6,15 +6,15 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import ru.grinin.winlineapp.presentation.screens.eventdetails.eventDetailsScreen
 import ru.grinin.winlineapp.presentation.screens.eventdetails.navigateToEventDetails
+import ru.grinin.winlineapp.presentation.screens.events.EventsRoute
 import ru.grinin.winlineapp.presentation.screens.events.eventsScreen
-import ru.grinin.winlineapp.presentation.screens.gifts.GiftsRoute
 import ru.grinin.winlineapp.presentation.screens.gifts.giftsScreen
 
 @Composable
 fun MainNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier,
-    startDestination: Any = GiftsRoute,
+    startDestination: Any = EventsRoute,
 ) {
     NavHost(
         navController = navController,
--- app/src/main/java/ru/grinin/winlineapp/presentation/screens/eventdetails/EventDetailsScreen.kt
+++ app/src/main/java/ru/grinin/winlineapp/presentation/screens/eventdetails/EventDetailsScreen.kt
@@ -1,9 +1,12 @@
 package ru.grinin.winlineapp.presentation.screens.eventdetails
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.repeatable
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,7 +18,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -23,12 +28,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -85,8 +94,13 @@ fun EventDetailsScreen(
 fun EventDetailsContent(
     event: EventDetailsVO
 ) {
+    val groupedMarkets = remember(event.markets) {
+        event.markets.groupBy { it.id }
+            .entries
+            .sortedBy { it.key }
+    }
     LazyColumn(
-        contentPadding = PaddingValues(16.dp),
+        contentPadding = PaddingValues(8.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         if (event.markets.isNotEmpty()) {
@@ -100,51 +114,76 @@ fun EventDetailsContent(
                 )
             }
 
-            items(
-                items = event.markets,
-                key = { it.uniqueKey }
-            ) { market ->
-                MarketCard(market = market)
+            items(groupedMarkets.size) { index ->
+                val (marketId, markets) = groupedMarkets[index]
+                MarketGroup(
+                    marketId = marketId,
+                    markets = markets,
+                    defaultExpanded = index < 10,
+                )
             }
         }
     }
 }
 
 @Composable
-private fun MarketCard(market: MarketDetailsVO) {
+private fun MarketGroup(
+    marketId: Int,
+    markets: List<MarketDetailsVO>,
+    defaultExpanded: Boolean = true,
+) {
+
+    var isExpanded by remember { mutableStateOf(defaultExpanded) }
     Card(
         modifier = Modifier.fillMaxWidth(),
         elevation = CardDefaults.cardElevation(defaultElevation = 1.dp),
-        colors = CardDefaults.cardColors(containerColor = Color.White)
+        colors = CardDefaults.cardColors(containerColor = Color.White),
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Text(
-                text = buildString {
-                    append(stringResource(R.string.market_id, market.id))
-                    if (market.specifiers.isNotEmpty()) {
-                        append(", ${market.specifiers}")
-                    }
-                },
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            )
-
-            Spacer(modifier = Modifier.height(12.dp))
-
+        Column {
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(12.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        isExpanded = !isExpanded
+                    }
+                    .padding(horizontal = 16.dp, vertical = 12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
-                market.outcomes.forEach { outcome ->
-                    OutcomeCard(
-                        outcome = outcome,
-                        modifier = Modifier.weight(1f)
-                    )
+                Text(
+                    text = marketId.toString(),
+                    fontSize = 16.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+
+                Image(
+                    modifier = Modifier,
+                    imageVector = if (isExpanded) Icons.Default.ExpandLess else Icons.Default.ExpandMore,
+                    contentDescription = null,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            AnimatedVisibility(isExpanded) {
+                markets.forEach { market ->
+
+                    Row(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        market.outcomes.forEach { outcome ->
+                            OutcomeCard(
+                                outcome = outcome,
+                                modifier = Modifier.weight(1f)
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
                 }
             }
         }
@@ -173,7 +212,7 @@ private fun OutcomeCard(
         modifier = modifier
             .height(64.dp)
             .clip(MaterialTheme.shapes.small)
-            .background(Color(0xFFF5F5F5))
+            .background(MaterialTheme.colorScheme.surfaceVariant)
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
